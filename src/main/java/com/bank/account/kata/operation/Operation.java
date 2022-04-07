f6634bbeba70a26541b6ec7f58a8780eb015a08d
TenniaYoussef <59965420+TenniaYoussef@users.noreply.github.com>
2022-04-07 10:02:50 +0200

    add project first files

---
+++ src/main/java/com/bank/account/kata/operation/Operation.java
@@ -0,0 +1,4 @@
+package com.bank.account.kata.operation;
+
+public class Operation {
+}
---
+++ src/test/java/com/bank/account/kata/operation/OperationTest.java
@@ -0,0 +1,7 @@
+package com.bank.account.kata.operation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationTest {
+
+}
