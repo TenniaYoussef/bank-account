8c4a5af6f486094434dd6f2f6dafa04571f99da5
TenniaYoussef <59965420+TenniaYoussef@users.noreply.github.com>
2022-04-07 10:15:45 +0200

    Adding operation tests [TDD - application]

--- src/main/java/com/bank/account/kata/operation/Operation.java
+++ src/main/java/com/bank/account/kata/operation/Operation.java
@@ -1,4 +1,10 @@
 package com.bank.account.kata.operation;
 
+import java.util.Date;
+
 public class Operation {
+
+    public Operation(OperationType operationType, Date operationDate, double amount) {
+    }
+
 }
--- src/test/java/com/bank/account/kata/operation/OperationTest.java
+++ src/test/java/com/bank/account/kata/operation/OperationTest.java
@@ -1,7 +1,22 @@
 package com.bank.account.kata.operation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 class OperationTest {
+
+    @Test
+    void should_throw_exception_when_date_is_null() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> new Operation(OperationType.DEPOSIT, null, 10));
+    }
+
+    @Test
+    void should_throw_exception_when_operation_type_is_null() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> new Operation(null, new Date(), 10));
+    }
 
 }
